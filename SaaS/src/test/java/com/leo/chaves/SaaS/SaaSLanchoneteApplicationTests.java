44446bfba7ffe7949af53cdd4d1ed1784e93f479
Leonardo Chaves <leonardo.2021107241@unicap.br>
2025-12-08 17:33:15 -0300

    versão 1.0

---
+++ SaaS/src/test/java/com/leo/chaves/SaaS/SaaSLanchoneteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.chaves.SaaS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaaSLanchoneteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
